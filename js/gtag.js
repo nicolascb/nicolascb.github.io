094a3a72377b8cb3959f8d722e37dc453a1f5df2
Nicolas Barbosa <ndevbarbosa@gmail.com>
2021-01-17 12:42:29 -0300

    update google analytics tag

---
+++ js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G9KMR7G27C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G9KMR7G27C');
+</script>
