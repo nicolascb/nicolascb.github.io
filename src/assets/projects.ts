33469c2abdc3bf0afe8803f868935d954a655133
Nicolas Barbosa <ndevbarbosa@gmail.com>
2025-09-08 08:35:50 -0300

    feat: add projects section

---
+++ src/assets/projects.ts
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: "nssh",
+    description: "Gerenciador de conexões SSH escrito em Go",
+    url: "https://github.com/nicolascb/nssh",
+    tags: ["Golang", "SSH", "CLI"],
+  },
+  {
+    title: "onmock.dev",
+    description: "Ferramenta online para desenvolvimento de mocks em APIs REST",
+    url: "https://onmock.dev",
+    tags: ["Golang", "API", "Tool"],
+  },
+  {
+    title: "camarada.vim",
+    description: "Estilo de tema para o editor vim",
+    url: "https://github.com/nicolascb/camarada.vim",
+    tags: ["Vim", "Theme"],
+  },
+  {
+    title: "vault-aws-provider",
+    description:
+      "Uma implementação de vault como provider para aws credentials em Go",
+    url: "https://github.com/nicolascb/vault-aws-provider",
+    tags: ["Golang", "AWS", "Vault"],
+  },
+];
